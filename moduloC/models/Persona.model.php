c3885684c6c105105b043e2413ccc42bef837c13
Jhon Eder <jhoneder453@gmail.com>
2023-04-26 11:48:05 -0500

    bajando cambios

--- moduloC/models/Persona.model.php
+++ moduloC/models/Persona.model.php
@@ -42,19 +42,14 @@ class Persona
 
 		function MostrarPersona($idpersonal)
 		{
-			$sql = "SELECT CONCAT(nombre,' ', apellidos) AS personal, nombre, apellidos, sexo, telefono,fecha_nac, DNI, foto, email, direccion FROM personal WHERE id_personal = $idpersonal;";
+			$sql = "SELECT CONCAT(nombre,' ', apellidos) AS personal, nombre, apellidos, sexo, telefono,fecha_nac, DNI, foto, email FROM personal WHERE id_personal = $idpersonal;";
 			$data = $this->conn->ConsultaArray($sql);
 			return $data;
 		}
-<<<<<<< HEAD
 		
-    function UpdatePersona($telefono, $fecha_nac, $email, $ruta, $idpersonal)
-=======
-
-    function UpdatePersona($telefono, $fecha_nac, $email, $ruta, $direccion, $idpersonal)
->>>>>>> 31bc32046a1e31027a5e705c0abc2bfd59671f51
+    	function UpdatePersona($telefono, $fecha_nac, $email, $ruta, $idpersonal)
 		{
-			$sql = "UPDATE personal SET telefono = '$telefono', fecha_nac = '$fecha_nac', email = '$email', foto = '$ruta', direccion = '$direccion' WHERE (id_personal = '$idpersonal');";
+			$sql = "UPDATE personal SET telefono = '$telefono', fecha_nac = '$fecha_nac', email = '$email', foto = '$ruta' WHERE (id_personal = '$idpersonal');";
 			$this->conn->ConsultaSin($sql);
 		}
 	}
